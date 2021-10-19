00118e704e66dfb20690f2167976ceb3671f1099
Cesar Cabrera <kenychy_17@hotmail.com>
2021-10-18 23:36:38 -0400

    Desarrollo Backend con SpringBoot y Frontend con React

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student marian =new Student(
+                    "Marian",
+                    "marian.jamal@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+            Student alex =new Student(
+
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+            Student carolina =new Student(
+                    "Carolina",
+                    "carolina@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+            repository.saveAll(
+                    List.of(marian, alex, carolina)
+            );
+        };
+    }
+}
